ffdda59011c2d8b550e14895c84c4846ebed45da
John Gergely <john.gergely@continuum.io>
2014-05-12 12:08:41 -0400

    Significant changes to environment creation. python conda-launch.py myapp.ipynb now serves the appified notebook through ipyapp/server_post.py. Paremters adjusted through GET and POST requests

--- conda-lauch.py
+++ conda-lauch.py
@@ -1,13 +1,19 @@
-#!/home/gergely/code/miniconda/bin/python
-
 import conda_api
 import subprocess
 import sys
 import os
+import signal
 import json
 import argparse
-#import server as ipyapp_server
-import webbrowser
+import ipyapp.server
+
+def parse_output(std_stream):
+	index = len(std_stream)-1
+	if index==1 and std_stream[index]:
+		print "ERROR MESSAGE"
+	else:
+		index = 0
+	print std_stream[index]
 
 def get_input_file():
 	# TODO need to accept tarballs containing package data, and have conda use those
@@ -16,7 +22,7 @@ def get_input_file():
 	p = argparse.ArgumentParser()
 	p.add_argument('filename')
 	p.add_argument('--envname','-e',nargs='?',dest='setenv')
-	p.add_argument('--mode','-m',nargs='?',default="ipynb", dest='setmode')
+	#p.add_argument('--mode','-m',nargs='?',default="ipynb", dest='setmode')
 	args = p.parse_args()
 
 	## handle input file options
@@ -33,13 +39,17 @@ def get_input_file():
 		print "Opened tarfile. Expecting to use notebook",appfile_ipynb
 		## need more graceful handling here to inspect tarball contents and identify notebooks
 	else:	
-		print "Input file",input_file,"has an extension that is not supported.\nSupported formats are\nipython notebook with extension .ipynb OR\ntarball containing ipython notebook of the same name"
+		print "Input file",input_file,'''
+		"has an extension that is not supported.
+		Supported formats are
+		ipython notebook with extension .ipynb OR
+		tarball containing ipython notebook of the same name'''
 		sys.exit(1)
-	return (appfile_ipynb, args.setenv, args.setmode)
+	#return (appfile_ipynb, args.setenv, args.setmode)
+	return (appfile_ipynb, args.setenv)
 
 def load_params(fname, override_params=None):
-	# default parameters here
-	set_params = {"depends" : ["ipython-notebook"],
+	set_params = {"depends" : [],
 								"platform_depends" : {},
 								"appname" : fname,
 								"envname" : "ipynb_test",
@@ -57,11 +67,6 @@ def load_params(fname, override_params=None):
 			print "Assigned parameter",param,set_params[param]
 	else:
 		print "No metadata furnished in notebook."
-	# this would be stuff from the command line
-	#if override_params:
-	#	for param in override_params.keys():
-	#		set_params[param] = override_params[param]
-	#		print "Overriding parameter",param,set_params[param]
 	print "Using parameters"
 	for param in set_params:
 		print param,":",set_params[param]
@@ -74,8 +79,21 @@ def issue_cmd(cmd_list, call_func=subprocess.check_output):
 	except:
 		print "Unable to execute command"," ".join(cmd_list)
 
+class ManageServer:
+	def __init__(self, launch_cmd):
+		#self.process = subprocess.Popen(launch_cmd.split(), preexec_fn=os.setsid)
+		self.process = subprocess.Popen(launch_cmd.split())
+		self.pid = self.process.pid
+		print "Launched process",self.pid
+
+	def terminate(self):
+		print "Terminating process",self.pid
+		#os.kill(self.pid, signal.SIGTERM)
+		self.process.terminate()
+		self.process.kill()
+
 if __name__=="__main__":
-	## linux only at this point
+	## Linux/ OS X only at this point
 
 	## set_root_prefix in conda-api
 	## TODO graceful error if conda not found
@@ -83,10 +101,15 @@ if __name__=="__main__":
 	path_stem = conda_path.split("/bin/conda")[0]
 	conda_api.set_root_prefix(path_stem)
 	
-	(appfile_ipynb, override_env, set_mode) = get_input_file()
+	#(appfile_ipynb, override_env, set_mode) = get_input_file()
+	(appfile_ipynb, override_env) = get_input_file()
 	## get metadata from ipynb (looking for key "app")
 	## use defaults where metadata are not provided
 	conda_params = load_params(appfile_ipynb)
+	## set any default pkg dependencies here (ipython, flask, etc)
+	depends_cmd = ['ipython','flask','runipy'] 
+	for add_pkg in conda_params['depends']:
+		depends_cmd.append(add_pkg)
 	# TODO could also try to be smart and look at import statements to suggest packages that should be installed
 	## TODO support version specification
 	## include a tuple for each package, version req optional
@@ -94,79 +117,58 @@ if __name__=="__main__":
 	if override_env:
 		conda_params['envname'] = override_env
 		print "Overriding envname from command line; using",conda_params['envname']
-	if set_mode == 'ipyapp':
-		conda_params['mode'] = set_mode	
-		print "ipyapp mode ****************************"
-		## should check here that input file is of the correct format in this case
-	else:
-		conda_params['mode'] = 'ipynb'
-		print "Default mode",conda_params['mode']
 
 	## create conda environment
+	## if default env exists add necessary packages to it; inform the user of this
+	## if the user specified an envname (override_env==True) we'll assume they
+	## have a deliberate configuration in mind and will create a unique envname if there is
+	## a conflict with an existing env name
+	env_list = conda_api.get_envs()
+	#print "Existing environments"
+	#print env_list
 	create_env = True
-	## first see if the name exists
-	## this is some clumsy string munging
-	env_output = issue_cmd(["info","-e"],conda_api._call_conda)[0].split("\n")
-	for envline in env_output:
-		if len(envline)>1:
-			envfound = envline.split()[0]
-			if envfound  == conda_params['envname']:
-				print "conda environment with the name",conda_params['envname'],"already exists with the following package info"
-				print issue_cmd(["list","-n",conda_params['envname']],conda_api._call_conda)[0]
-				## TODO here the user should be given a choice to use
-				## use the found environment
-				## OR create a new one with another name
-				## we should compare dependencies against the env list
-				## and notify of those missing
-				print "For now, we'll just use this env and hope it's right for you."
-				create_env = False
-				break
+	# envname already exists
+	if conda_params['envname'] in [env_name.split('/')[-1] for env_name in env_list]:
+		print "conda environment with the name",conda_params['envname'],"already exists with the following package info"
+		parse_output(issue_cmd(["list","-n",conda_params['envname']],conda_api._call_conda))
+		if override_env:
+			new_envname = conda_params['envname']
+			incr = 1
+			while new_envname in [env_name.split('/')[-1] for env_name in env_list]:
+				new_envname = conda_params['envname'] + '_ipyapp_' + str(incr)
+				incr += 1
+			print "Generated new environment name",new_envname
+			conda_params['envname'] = new_envname
+		else: # if necessary add pkgs to existing env
+			create_env = False
+			print "Installing any relevant packages into existing env"
+			conda_config_env_cmd = ['install','-n',conda_params['envname']] + depends_cmd
+			print "installing conda packages",depends_cmd
+			parse_output(issue_cmd(conda_config_env_cmd, conda_api._call_conda))
 	if create_env:
-		## assemble the command string to pass to conda
-		conda_create_env_cmd = ['create','-n',conda_params['envname']]
-		for add_pkg in conda_params['depends']:
-			conda_create_env_cmd.append(add_pkg)
-			#add_str = add_pkg[0]
-			#if len(add_pkg) > 1:
-			#	add_str += "=" + str(add_pkg[1])
-			#conda_create_env_cmd.append(add_str)
-
+		conda_create_env_cmd = ['create','-n',conda_params['envname']] + depends_cmd
 		print "creating conda environment:"	
 		print conda_create_env_cmd
+		parse_output(issue_cmd(conda_create_env_cmd, conda_api._call_conda))
 		print "Hit enter to accept proposed package plan (it is not displayed to you at the moment)."
-		## awkward hack here: user doesn't get to interact with conda create and approve the environment
-		comm = issue_cmd(conda_create_env_cmd, conda_api._call_conda)
-		print comm
+		## TODO awkward hack here: user doesn't get to interact with conda create and approve the environment
 
 	## need to adjust $PATH to find our environment
 	## I think this has to be done by hand...?
-	## how to do this in a cross-platform way?
+	## need to do this in a cross-platform way
 	print "activating environment",conda_params['envname']
 	add_path = path_stem + "/envs/" + conda_params['envname'] + "/bin"
 	curr_path = os.environ['PATH']
 	print "adding path",add_path
 	os.environ['PATH'] = add_path + ":" + os.environ['PATH']
-	print "checking for ipython"
-	comm = issue_cmd(['which','ipython'],subprocess.call)
-	## need to handle cases where ipython not found or environment was not set up correctly...
 
-	if conda_params['mode'] == 'ipyapp':
-		#ipyapp_server.start()	
-		launch_app_server = ['python','ipyapp/server.py','&']
-		subprocess.Popen(launch_app_server)
-		app_stem = appfile_ipynb.split(".")[0]
-		ipyapp_url = "127.0.0.1:5000/ipyapp/" + app_stem
-		print "Opening app at",ipyapp_url
-		webbrowser.open(ipyapp_url)
-
-	## default is just open notebook
-	else:
-		## launch ipynb
-		print "launching..."
-		run_cmd = ['ipython','notebook',appfile_ipynb]
-		comm = issue_cmd(run_cmd)
-		print comm
+	set_host = "127.0.0.1"
+	set_port = 5000
+	print "Launching app server"
+	print "GET access at http://"+set_host+":"+str(set_port)+"/"+appfile_ipynb
+	print "POST form at http://"+set_host+":"+str(set_port)+"/"+appfile_ipynb+"/form"
+	#subprocess.call(["python","ipyapp/server.py"])
+	subprocess.call(["python","ipyapp/server_post.py"])
 
 	## clean up
 	os.environ['PATH'] = curr_path
-	## more graceful exit here would be nice?
