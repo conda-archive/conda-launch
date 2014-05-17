a18cfca860c30e1d9c3ae2d06bdbde38bf9d4275
Ian Stokes-Rees <ijstokes@continuum.io>
2014-05-17 14:11:48 -0400

    moved example ipynb files into examples dir, and simplified to one server.py (formerly server_post.py) and one client.py (formerly conda-launch.py)

---
+++ USECASES.md
@@ -0,0 +1,21 @@
+Eric is our end user.  Eric doesn't know any Python.  He wants to see Python in
+his browser as much as he wants to see PHP in his browser.
+
+Paula is our Pythonista.  Paula isn't a web developer, or even an app
+developer, but she loves IPython Notebooks and creates ones that she would like
+to be able to share as "graphical scripts" with her colleagues, like Eric.
+Those colleagues want to run them but not have to look at or touch any code.
+
+
+
+Simple app view of a notebook
+-----------------------------
+
+Eric: `conda launch "Power Series.ipynb"`
+
+*end user:*
+
+* Eric's default browser opens up with an input form asking for the parameters
+  required to run the notebook `Power Series.ipynb`
+
+*   
--- ipyapp/server.py
+++ ipyapp/server.py
@@ -1,11 +1,11 @@
-import json 
+import json
 from StringIO import StringIO
 
 from IPython.config import Config
 from IPython.nbconvert.exporters.html import HTMLExporter
 from IPython.nbformat.current import read as nb_read, write
 
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
 
 from werkzeug.exceptions import BadRequestKeyError
 
@@ -13,12 +13,27 @@ from runipy.notebook_runner import NotebookRunner, NotebookError
 
 app = Flask(__name__, template_folder='templates')
 
+@app.route("/custom.css")
 @app.route("/ipyapp/custom.css")
 def custom_css():
     return ""
 
-@app.route("/ipyapp/<nbname>")
-def nblaunch(nbname):
+## form
+@app.route("/<nbname>.ipynb/form")
+@app.route("/<nbname>/form")
+def nb_form(nbname):
+    params = {}
+    nb = json.load(open(nbname + ".ipynb"))
+    app_meta = json.loads("".join(nb['worksheets'][0]['cells'][-1]['source']))
+    for var, t in app_meta['inputs'].iteritems():
+        params[var] = None
+    return render_template("form_submit.html", params = params, nbname = nbname)
+
+## post
+@app.route("/<nbname>.ipynb", methods=['GET','POST'])
+@app.route("/<nbname>", methods=['GET','POST'])
+def nb_post(nbname):
+    print "You may submit new parameters using",url_for('nb_form', nbname=nbname)
     input_cell = json.loads("""
     { 
      "cell_type": "code",
@@ -30,15 +45,17 @@ def nblaunch(nbname):
      "prompt_number": 3
     }
 """)
-
     nb = json.load(open(nbname + ".ipynb"))
-    # TODO: nb['worksheets'][0]['cells'][-1]['cell_type'] == 'raw' etc. etc.
     app_meta = json.loads("".join(nb['worksheets'][0]['cells'][-1]['source']))
     for var, t in app_meta['inputs'].iteritems():
         try:
-            value = eval("repr({cast}('{expr}'))".format(cast=t, expr=request.args[var]))
+            if request.method == 'GET':
+              value = eval("repr({cast}('{expr}'))".format(cast=t, expr=request.args[var]))
+            else:
+              value = eval("repr({cast}('{expr}'))".format(cast=t, expr=request.form[var]))
         except BadRequestKeyError as ex:
             value = eval("repr({cast}())".format(cast=t))
+            print "BadReq for parameter key/type",var,t
         input_cell['input'].append('{var} = {value}\n'.format(var=var, value=value))
         
     nb['worksheets'][0]['cells'][0] = input_cell
@@ -46,7 +63,6 @@ def nblaunch(nbname):
     nb_obj = nb_read(StringIO(json.dumps(nb)), 'json')
     nb_runner = NotebookRunner(nb_obj)
     nb_runner.run_notebook(skip_exceptions=False)
-
     exporter = CustomHTMLExporter(config=Config({'HTMLExporter':{'default_template': 'noinputs.tpl'}}))
     output, resources = exporter.from_notebook_node(nb_runner.nb)
     return output
@@ -60,7 +76,7 @@ class AppifyNotebook(Preprocessor):
         if hasattr(cell, "prompt_number"):
             del cell.dict()['prompt_number']
         if hasattr(cell, "input"):
-            del cell.dict()['input']
+          del cell.dict()['input']
         return cell, resources
 
 class CustomHTMLExporter(HTMLExporter):
@@ -71,4 +87,3 @@ class CustomHTMLExporter(HTMLExporter):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
